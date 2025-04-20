4ee1e9a92f3e6b3460f1a8737bd9b44302d4625c
intern-tan.nd <intern-tan.nd@codecomplete.jp>
2025-04-20 22:24:00 +0700

    fix duplicate export in utils/axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getCookieToken } from "./cookie";
 
-export const AI_api = axios.create({
+const AI_api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_AI_URL || "http://localhost:5000",
     headers: {
         "Content-Type": "application/json",
